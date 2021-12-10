1c31ecfda2e95c7badaa5d644149ed047543ba0d
[refrrir] <[liu.qihu@northeatern.edu]>
2021-12-09 19:51:14 -0800

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ process.env.SUPER_DUPER_SECRET = "my_secret_ssshhhhh_1234567890";
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     console.log("received request");
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
